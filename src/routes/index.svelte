cf9787b4990e25e04daa8494887ead3c66ae3174
rkiel <cygnus@pobox.com>
2021-12-31 11:57:52 -0500

    tabs: started tabs

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Inspired by...</h1>
+<h2>
+	<a href="https://www.better.dev/courses/make-10-react-apps">Make 10 React Apps</a>
+</h2>
+
+<p>Chris Sev is a great teacher. Highly recommend any of his courses!</p>
+
+<h2>Series A</h2>
+<ul>
+	<li><a href="/pomodoro">Pomodoro</a></li>
+	<li><a href="/markdown">Markdown</a></li>
+	<li><a href="/tabs">Tabs</a></li>
+</ul>

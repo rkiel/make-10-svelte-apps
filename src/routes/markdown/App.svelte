691f871d773993161dbf10f3db6d53d46736ddd0
rkiel <cygnus@pobox.com>
2021-12-31 10:13:24 -0500

    markdown: added markdown

---
+++ src/routes/markdown/App.svelte
@@ -0,0 +1,40 @@
+<script>
+	import { marked } from 'marked';
+	import SvelteMarkdown from 'svelte-markdown';
+
+	let markdown = '# sup';
+</script>
+
+<div class="columns">
+	<textarea bind:value={markdown} class="editor" />
+
+	<div>
+		<h1>Using marked</h1>
+		<div class="marked">
+			<div>{@html marked(markdown)}</div>
+		</div>
+
+		<h1>Using svelte-markdown</h1>
+		<div class="svelte-markdown">
+			<SvelteMarkdown source={markdown} />
+		</div>
+	</div>
+</div>
+
+<style>
+	.columns {
+		display: flex;
+		padding: 5rem;
+	}
+	.editor {
+		margin-right: 3rem;
+	}
+	.marked {
+		padding: 3rem;
+		background: #eee;
+	}
+	.svelte-markdown {
+		padding: 3rem;
+		background: #aaa;
+	}
+</style>
